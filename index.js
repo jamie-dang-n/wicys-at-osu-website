958bfb34307132aa786e6a4a194974f52c47700d
jaywhite0101 <whitejo8@oregonstate.edu>
2024-11-26 15:21:04 -0800

    testimonial's page basics

---
+++ index.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.testimonial-slide');
+let currentSlide = 0;
+
+document.querySelector('.next-slide').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
+
+document.querySelector('.prev-slide').addEventListener('click', () => {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+});
