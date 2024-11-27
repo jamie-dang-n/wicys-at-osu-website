e7a188e671cfdd3bb802cf0514539261377fd26a
Jamie D. <45217971+jamie-dang-n@users.noreply.github.com>
2024-11-26 16:21:11 -0800

    Templatized index.html, created main layout and homePage view

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var path = require('path')
 var express = require('express')
+var exphbs = require('express-handlebars')
 
 var app = express()
 var port = process.env.PORT || 3000
 
 app.use(express.static('static'))
 
+app.engine("handlebars", exphbs.engine({
+    defaultLayout: "main"
+}))
+
+app.set("view engine", "handlebars")
+
+app.use(express.static('static'))
+
+// Display Homepage
+app.get('', function (req, res, next) {
+    res.status(200).render("homePage")
+})
+
+
+
 app.listen(port, function () {
     console.log("== Server is listening on port", port)
 })
+
