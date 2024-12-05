2dc3372edf25b7b2f99239620e05c03a067ace74
Alexa Baruela <baruelaa@oregonstate.edu>
2024-12-04 16:58:58 -0800

    added clear adn alert functions

--- static/index.js
+++ static/index.js
@@ -15,3 +15,33 @@ document.querySelector('.prev-slide').addEventListener('click', () => {
     currentSlide = (currentSlide - 1 + slides.length) % slides.length;
     slides[currentSlide].classList.add('active');
 });
+
+
+/* CONTACTS FORM */
+var nameVal = document.getElementById("contactName");
+var email = document.getElementById("contactEmail");
+var phone = document.getElementById("contactPhone");
+var message = document.getElementById("contactInput");
+
+//taking code from assignment 3/5 for modal and text clearing
+function openModal(){
+    document.getElementById("contacts-modal").classList.remove("hidden");
+    document.getElementById("modal-backdrop").classList.remove("hidden");
+}
+
+document.getElementById("contacts-button").onclick = openModal;
+
+function clearInput(){
+    nameVal = '';
+    email = '';
+    phone = '';
+    message = '';
+}
+
+function contactAlert(){
+    if (nameVal.value == '' || email.value == '' || phone.value == '' || message.value == ''){
+        alert('All fields must be completed');
+    }
+
+    //add this information to a database
+}
