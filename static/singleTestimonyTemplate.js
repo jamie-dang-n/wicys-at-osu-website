eb5595dc681d3a8ddbe1553dc0945c5d0083a0b5
wileypa <wileypa@oregonstate.edu>
2024-12-07 19:28:13 -0800

    testimony is ready to push

--- static/singleTestimonyTemplate.js
+++ static/singleTestimonyTemplate.js
@@ -10,8 +10,8 @@ templates['singleTestimony'] = template({"compiler":[8,">= 4.3.0"],"main":functi
 
   return "\r\n<div class=\"testimonial-post\">\r\n    <div class=\"testimonial-content\">\r\n        <div class=\"testimony-text-col\">\r\n            <div class=\"testimony-text\">\r\n                <h2>"
     + alias4(((helper = (helper = lookupProperty(helpers,"name") || (depth0 != null ? lookupProperty(depth0,"name") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data,"loc":{"start":{"line":6,"column":20},"end":{"line":6,"column":28}}}) : helper)))
-    + "</h2>\r\n                <p>"
-    + alias4(((helper = (helper = lookupProperty(helpers,"desc") || (depth0 != null ? lookupProperty(depth0,"desc") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"desc","hash":{},"data":data,"loc":{"start":{"line":7,"column":19},"end":{"line":7,"column":27}}}) : helper)))
+    + "</h2>\r\n                <p class=\"testimony-desc\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"desc") || (depth0 != null ? lookupProperty(depth0,"desc") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"desc","hash":{},"data":data,"loc":{"start":{"line":7,"column":42},"end":{"line":7,"column":50}}}) : helper)))
     + "</p>\r\n            </div>\r\n            <button type=\"button\" class=\"button\" id=\"readMore\">Read More</button>\r\n        </div>\r\n        <div class=\"testimony-pic\">\r\n            <img src=\""
     + alias4(((helper = (helper = lookupProperty(helpers,"url") || (depth0 != null ? lookupProperty(depth0,"url") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data,"loc":{"start":{"line":12,"column":22},"end":{"line":12,"column":29}}}) : helper)))
     + "\" alt=\""
